6fff486b8af018a6db8c9e110de8852cdad3fad4
UnusualFrog <forwardnoah@gmail.com>
2026-05-30 19:24:33 -0230

    feat: dataset is split between training and target features; irrelevant features are dropped

--- src/baseline_corrected.py
+++ src/baseline_corrected.py
@@ -1,1 +1,22 @@
+import random
 
+import pandas as pd 
+import numpy as np
+import torch
+import tensorflow as tf
+
+# Set Global Random Seed for reproducability
+GLOBAL_SEED = 42
+random.seed(GLOBAL_SEED)
+np.random.seed(GLOBAL_SEED)
+tf.random.set_seed(GLOBAL_SEED)
+torch.manual_seed(GLOBAL_SEED)
+torch.cuda.manual_seed_all(GLOBAL_SEED)
+
+
+# Read raw dataset
+df = pd.read_csv('data/Reviews.csv')
+
+# Display dataset info and sample rows
+df.info()
+print(df.head(3))
--- src/baseline_flawed.py
+++ src/baseline_flawed.py
@@ -1,9 +1,12 @@
-import pandas as pd 
-import numpy as np 
+import random
 
-# Set Global Random Seed for reproducability
-GLOBAL_SEED = 42;
-np.random.seed(GLOBAL_SEED)
+import pandas as pd 
+import numpy as np
+import torch
+import tensorflow as tf
+
+from imblearn.over_sampling import SMOTE
+
 
 # Read raw dataset
 df = pd.read_csv('data/Reviews.csv')
@@ -11,3 +14,21 @@ df = pd.read_csv('data/Reviews.csv')
 # Display dataset info and sample rows
 df.info()
 print(df.head(3))
+
+# Drop irrelevant features
+df = df.drop(["Id", "ProductId", "UserId", "ProfileName", "HelpfulnessNumerator", "HelpfulnessDenominator", "Time", "Summary"], axis=1)
+
+# Split training features from target feature
+X = df.drop("Score", axis=1)
+y = df["Score"]
+
+print(X.head(3))
+print(y.head(3))
+
+
+# Apply SMOTE before split (generally incorrect, but accurate replication)
+# smote = SMOTE(sampling_strategy='minority', random_state=42)
+# X,y = smote.fit_resample(X,y)
+
+# print(X.head(3))
+# print(y.head(3))
